48949ab1a4687d3760faa9f2ce66507c9f2e137f
akhterjawad <akhterjawad566@gmail.com>
2024-02-27 21:32:24 +0500

    JS file

---
+++ app.js
@@ -0,0 +1,29 @@
+// alert("hellow")
+
+
+// 4 tarha ki data types hoti ha
+
+// 1 string "ejdsn4383029"
+// 2 number 6738
+// 3Boolean true ya false
+// 4 ko badma dakhanga
+var userName = ("jawad")
+var age = (19)
+var education = ("X!!")
+var address = ("Malir kalaboard")
+console.log(userName)
+console.log(age)
+console.log(education)
+console.log(address)
+console.log(userName + " " + age + " " + education + " " + address)
+var Age1 = 62;
+var Age2 = 63;
+var Age3 = "64";
+var Age4 = "65";
+console.log( Age2 + Age1 )
+console.log( Age2 + " " + Age1 )
+console.log( Age3 + " " + Age4 )
+console.log( Age3 + + Age4 )
+
+
+
