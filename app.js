849467ddcd38be7f90fd3b9ceaac35c3c6ba6cb9
akhterjawad <akhterjawad566@gmail.com>
2024-02-28 07:48:30 +0500

    JS work

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // alert("hellow")
-
+// alert("jawad \ndream")
 
 // 4 tarha ki data types hoti ha
 
@@ -11,25 +11,54 @@ var userName = ("jawad")
 var age = (19)
 var education = ("X!!")
 var address = ("Malir kalaboard")
-console.log(userName)
-console.log(age)
-console.log(education)
-console.log(address)
+console.log(userName);
+console.log(age);
+console.log(education);
+console.log(address);
 console.log(userName + " " + age + " " + education + " " + address)
 var Age1 = 62;
 var Age2 = 63;
 var Age3 = "64";
 var Age4 = "65";
-console.log( Age2 + Age1 )
-console.log( Age2 + " " + Age1 )
-console.log( Age3 + " " + Age4 )
-console.log( Age3 + + Age4 )
+console.log(Age2 + Age1);
+console.log(Age2 + " " + Age1);
+console.log(Age3 + " " + Age4);
+console.log(Age3 + + Age4);
 // var Age1 = prompt("enter first number");
 // var Age2 = prompt("enter second number");
 // var Age3 = +prompt("enter THIRD number");
 // var Age4 = +prompt("enter fourth number");
 console.log(Age1 + Age2);
 console.log(Age3 + Age4);
+var num1 = 20;
+// num1=num1+10
+// line number 34 ka 1 short form bhi ha wo nicha likhi ha
+num1 += 10
+// num1-=10
+// num1*=10
+// num1/=10
+console.log(num1 + 15);
+console.log(10 < 5);
+console.log(10 >= 10);
+console.log(10 <= 10);
+console.log(10 >= "10");
+console.log(9 <= "10");
+// xclination mark (!) ka madlab ha not
+console.log(10 != 10);
+console.log(10 !== "10");
+// double equal to data type bhi check krta ha (==) LockManager(!) ka sath
+// && = and
+// || = OR
+var comparision = 10 <= 10 || 10 < 6;
+console(comparision)
+// (|| = OR)jitni bhi conditions ho unma sa agar 1 bhi true hui to wo nicha true batada ga answer ma
+// (& = and) is parallel of OR
+
+
+
+
+
+
 
 
 
