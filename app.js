d66e23cdb66e45e0bd3a938794b0f64b07b9330b
akhterjawad <akhterjawad566@gmail.com>
2024-02-29 22:44:38 +0500

    if && else

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ console.log(comparision);
 
 // var num = +prompt("number dal da")
 // if(num > 5 ){
-//     console.log("ifi") 
+//     console.log("ifi")
 // }
 // else{
 //     console.log('elses')
@@ -108,8 +108,51 @@ console.log(comparision);
 
 
 
+// var height = +prompt("Put the height")
+// if(height < 20){
+//     console.log("not accept")
+// }
+// else if( height > 30 ){
+//   console.log("Accepted")
+// }
+// else{
+//     console.log("just accepted")
+// }
 
 
 
+// var salery = +prompt("Please tell me a salery")
+// if(salery >= 100000){
+//     console.log("I will convince my father")
+// }
+// else if(salery<=80000){
+//    console.log("my father will never agree")
+// }
+// else{
+//     console.log("maybe my father will accept")
+// }
+
+// var Age = +prompt("Enter your age" )
+// if( Age >= 28 ){
+//     console.log(" abba nahi manga")
+// }
+// else if( 22 <= Age  ){
+//    console.log("ma abba ko manalongi ")
+// }
+// else if( 18 <= Age  ){
+//     console.log("bs thora bara hojaw ")
+//  }
+// else{
+//     console.log("bara ho ka a ")
+// }
+
+// var num = +prompt("code dala")
+// if( num > 100 ){
+//     console.log("code sahi ha")
+// }
+
+// else{
+//     console.log("code ghalat ha")
+// }
 
 
